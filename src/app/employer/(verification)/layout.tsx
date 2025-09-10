19639fff74b3fd771ffa0e166244ee650dae2860
deverana <developerrana0509@gmail.com>
2025-09-10 10:26:33 +0530

    added protected route in employer

--- src/app/employer/(verification)/layout.tsx
+++ src/app/employer/(verification)/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import ProtectedRoute from "@/lib/auth/ProtectedRoute";
 import React, { ReactNode } from "react";
 
 export default function EmployerVerificationLayout({ children }: { children: ReactNode }) {
     return (
-        <div >
-            {children}
-        </div>
+        <ProtectedRoute roles={["EMPLOYER"]}>
+            <div >
+                {children}
+            </div>
+        </ProtectedRoute>
     );
 }
